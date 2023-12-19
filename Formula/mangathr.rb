38fae1d43e6e543e39faed7d0123354f46365932
Luke Browning <24992944+browningluke@users.noreply.github.com>
2023-12-18 22:12:47 -0800

    Add mangathr v2.2.0 (#1)
    * Add mangathr v2.2.0
    
    * Fix style issue
    
    * Fix style issue
    
    * Fix style issue

---
+++ Formula/mangathr.rb
@@ -0,0 +1,23 @@
+class Mangathr < Formula
+  desc "Terminal utility for downloading manga with support for MangaDex & Cubari"
+  homepage "https://github.com/browningluke/mangathr"
+  url "https://github.com/browningluke/mangathr.git",
+      tag:      "v2.2.0",
+      revision: "a41ac08cc831245726570662f6537fc8f83e3974"
+  license "MIT"
+  head "https://github.com/browningluke/mangathr.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "bin/mangathr", "./cmd/mangathr"
+    bin.install "bin/mangathr"
+
+    generate_completions_from_executable(bin/"mangathr", "completion")
+  end
+
+  test do
+    version_output = shell_output(bin/"mangathr version 2>&1")
+    assert_match "v#{version}", version_output
+  end
+end
